27936731a87f8e09e6bc2e14e67520c4de6cfd7a
Phillip Geltman <phillipgeltman@Phillips-MacBook-Pro.local>
2019-06-04 22:48:45 -0700

    finished wedding invites!

--- excercises/weddingInvites/index.js
+++ excercises/weddingInvites/index.js
@@ -1,7 +1,12 @@
 //Problem: Wedding invites
 //Inputs: An array of guests coming to a party
 //Output: A tabular output that semantically has identified each element in the name
+
+//To Dos: Can we block the last name if the person doesn't have a guest?
+//can we put an indicator next to spouse first name so that we know which one's we need to get
+
 import guestList from './guestList';
+import fs from 'fs';
 
 class ZolaRow {
   constructor() {
@@ -14,68 +19,115 @@ class ZolaRow {
 
 class Person {
   constructor() {
-    this.title = '[title]';
-    this.firstName = '[first]';
-    this.lastName = '[last]';
+    this.title = '';
+    this.firstName = '';
+    this.lastName = '';
   }
 }
 
-function decodeWord(word) {
-  word = word.toLowerCase();
-
-  if (word === '&' || word === 'and') {
-    return 'a';
-  } else if (word === 'guest') {
-    return 'g';
-  } else if (word === 'family') {
-    return 'f';
-  } else if (word.endsWith('.') === true) {
-    return 't';
-  } else {
-    return 'n';
+function wordType(word) {
+  if (word !== undefined) {
+    word = word.toLowerCase();
+    if (word === '&' || word === 'and') {
+      return 'and';
+    } else if (word === 'guest') {
+      return 'guest';
+    } else if (word === 'family') {
+      return 'family';
+    } else if (word.endsWith('.') === true) {
+      return 'title';
+    } else {
+      return 'name';
+    }
   }
 }
 
 function nameID(name) {
   let row = new ZolaRow();
-
   const sections = name.split(' ');
 
-  let pattern = '';
-
-  sections.forEach(word => {
-    pattern += decodeWord(word);
-  });
-
-  //console.log(pattern + ': ' + name);
-
-  //tnn
-  if (/^tn+$/.test(pattern)) {
-  } else if (/^tn+ag$/.test(pattern)) {
-    //console.log('tnnag action');
-  } else if (/^tn+atn+$/.test(pattern)) {
-    //console.log('tnnatnn action');
-  } else if (/^tatn+$/.test(pattern)) {
-    //console.log('tatnn action');
-  } else if (/^tatn+af$/.test(pattern)) {
-    //console.log('tatnnaf action');
-  } else {
-    console.log(`unknown (${pattern}): ${name}`);
+  if (sections.length === 1) {
+    console.log(sections);
+    return row;
   }
 
-  for (let i = 0; i < pattern.length; i++) {}
+  let title, person;
+  title = person = 'main';
+  let currentName = '';
+
+  for (let i = 0; i < sections.length; i++) {
+    let word = sections[i];
+    let nextWord = sections[i + 1];
+
+    if (wordType(word) === 'and') {
+      //do nothing
+    } else if (wordType(word) === 'guest') {
+      row.andGuest = true;
+    } else if (wordType(word) === 'family') {
+      row.andFamily = true;
+    } else if (wordType(word) === 'title') {
+      if (word === 'Drs.') {
+        row.main.title = 'Dr.';
+        row.partner.title = 'Dr.';
+      } else if (title === 'main') {
+        row.main.title = word;
+        title = 'partner';
+      } else if (title === 'partner') {
+        row.partner.title = word;
+      } else console.log(title);
+    } else if (wordType(word) === 'name') {
+      if (wordType(nextWord) === 'name') {
+        currentName += word + ' ';
+      } else {
+        if (person === 'main') {
+          row.main.firstName = currentName;
+          row.main.lastName = word;
+          //row.partner.lastName = word;
+          person = 'partner';
+        } else {
+          row.partner.firstName = currentName;
+          row.partner.lastName = word;
+        }
+
+        currentName = '';
+      }
+    }
+  }
+
+  if (row.partner.title !== '' && row.partner.firstName === '') {
+    row.partner.firstName = '[Spouse]';
+    row.partner.lastName = row.main.lastName;
+  }
 
   return row;
 }
 
+function zolafy(o) {
+  return `${
+    o.main.title
+  },${o.main.firstName.trim()},${o.main.lastName.trim()},,${
+    o.partner.title
+  },${o.partner.firstName.trim()},${o.partner.lastName.trim()},,${
+    o.andGuest ? 'yes' : ''
+  },,,,,,,,,${o.andFamily ? '[Child]' : ''},${
+    o.andFamily ? o.main.lastName.trim() : ''
+  }`;
+}
+
 export { nameID };
 
-nameID('Ms. Brooke Briseno'); //tnn
-nameID('Ms. Ashlyn Rogers & Guest'); //tnnag
-nameID('Mr. Michael Gould and Ms. Sandra Badillo'); //tnnatnn
-nameID('Mr. And Mrs. Kevin Murray'); //tatnn
-nameID('Mr. and Mrs. Jeremy Geltman & Family'); //tatnnaf
+let final = '';
+
+//final = nameID('Ms. Brooke Briseno');
+//final = nameID('Ms. Ashlyn Rogers & Guest');
+//final = nameID('Mr. Michael Gould and Ms. Sandra Badillo');
+//final = nameID('Mr. And Mrs. Kevin Murray');
+//final = nameID('Mr. and Mrs. Jeremy Geltman & Family');
+
+console.log(final);
 
 guestList.forEach(name => {
-  nameID(name.trim());
+  let guestObject = nameID(name.trim());
+  let guestRow = zolafy(guestObject);
+  console.log(guestRow);
 });
--- test/weddingInvites.js
+++ test/weddingInvites.js
@@ -5,22 +5,22 @@ const should = require('chai').should();
 describe('Wedding Invites', () => {
   it('Blank name', () => {
     const name = nameID('');
-    name.main.title.should.equal('[title]');
-    name.main.firstName.should.equal('[first]');
-    name.main.lastName.should.equal('[last]');
-    name.partner.title.should.equal('[title]');
-    name.partner.firstName.should.equal('[first]');
-    name.partner.lastName.should.equal('[last]');
+    name.main.title.should.equal('');
+    name.main.firstName.should.equal('');
+    name.main.lastName.should.equal('');
+    name.partner.title.should.equal('');
+    name.partner.firstName.should.equal('');
+    name.partner.lastName.should.equal('');
     name.andGuest.should.equal(false);
     name.andFamily.should.equal(false);
   });
   it('Title', () => {
     const name = nameID('Ms. Brooke Briseno');
-    name.main.title.should.equal('Ms');
+    name.main.title.should.equal('Ms.');
   });
   it('Single Person', () => {
     const name = nameID('Ms. Brooke Briseno');
-    name.main.title.should.equal('Ms');
+    name.main.title.should.equal('Ms.');
     name.main.firstName.should.equal('Brooke');
     name.main.lastName.should.equal('Briseno');
   });
@@ -28,23 +28,23 @@ describe('Wedding Invites', () => {
     const name = nameID('Ms. Ashlyn Rogers & Guest');
     name.andGuest.should.equal(true);
   });
-  it.skip('two named people', () => {
+  it('two named people', () => {
     const name = nameID('Mr. Michael Gould and Ms. Sandra Badillo');
-    name.main.title.should.equal('Mr');
+    name.main.title.should.equal('Mr.');
     name.main.firstName.should.equal('Michael');
     name.main.lastName.should.equal('Gould');
-    name.partner.title.should.equal('Ms');
+    name.partner.title.should.equal('Ms.');
     name.partner.firstName.should.equal('Sandra');
     name.partner.lastName.should.equal('Badillo');
     name.andGuest.should.equal(false);
     name.andFamily.should.equal(false);
   });
-  it.skip('named couple', () => {
+  it('named couple', () => {
     const name = nameID('Mr. And Mrs. Kevin Murray');
-    name.main.title.should.equal('Mr');
+    name.main.title.should.equal('Mr.');
     name.main.firstName.should.equal('Kevin');
     name.main.lastName.should.equal('Murray');
-    name.partner.title.should.equal('Mrs');
+    name.partner.title.should.equal('Mrs.');
     name.partner.lastName.should.equal('Murray');
     name.andGuest.should.equal(false);
     name.andFamily.should.equal(false);
@@ -55,10 +55,10 @@ describe('Wedding Invites', () => {
   });
   it.skip('Edge Case', () => {
     const name = nameID('Drs. Daniel and Janine Heller');
-    name.main.title.should.equal('Dr');
+    name.main.title.should.equal('Dr.');
     name.main.firstName.should.equal('Daniel');
     name.main.lastName.should.equal('Heller');
-    name.partner.title.should.equal('Dr');
+    name.partner.title.should.equal('Dr.');
     name.partner.firstName.should.equal('Janine');
     name.partner.lastName.should.equal('Heller');
     name.andGuest.should.equal(false);
